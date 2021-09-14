5e5497e5c3d6cbf015341eb33103f528758bbc3e
Rodrigo Nogueira <rdg6design@gmail.com>
2021-09-14 11:36:44 -0300

    backend send e-mail

--- server.js
+++ server.js
@@ -6,23 +6,10 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express()
+
 const port = 3000
 
 const dirPath = path.join(__dirname, '/images');
-
-var arr = [];
-
-var files = fs.readdirSync(dirPath);
-
-files.forEach(file => {
-  let fileStat = fs.statSync(dirPath + '/' + file).isDirectory();
-  if(!fileStat) {
-    arr.push(file);
-  }
-});
-
-console.log(arr,'meu array');
-console.log(arr.toString(),'meu array');
 
 const fileStorageengine = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -38,49 +25,54 @@ const upload = multer({ storage: fileStorageengine });
 console.log(upload, "upload")
 
 app.post('/single', upload.single('image'), (req, res)=> {
-    console.log(req.file)
+  const titulo = req.body.title
+  console.log(titulo)
+  const getMostRecentFile = (dirPath) => {
+    const files = orderRecentFiles(dirPath);
+    return files.length ? files[0] : undefined;
+  };
+
+  const orderRecentFiles = (dirPath) => {
+    return fs
+      .readdirSync(dirPath)
+      .filter((file) => fs.lstatSync(path.join(dirPath, file)).isFile())
+      .map((file) => ({ file, mtime: fs.lstatSync(path.join(dirPath, file)).mtime }))
+      .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+  };
+
+  const arr = getMostRecentFile(dirPath).file
+  
+  console.log(getMostRecentFile(dirPath).file, 'file')
+  console.log(getMostRecentFile(dirPath).mtime, 'mtime')
+
+    const  transporter = nodemailer.createTransport({
+      service: SMTP_CONFIG.service,
+        auth: {
+          user: SMTP_CONFIG.user,
+          pass: SMTP_CONFIG.pass
+        }
+    });
+    
+  const email = {
+    from: SMTP_CONFIG.user,
+    to: 'rdg6design@gmail.com',
+    subject: titulo,
+    text: titulo,
+    html: `<h1>Rodrigo Nogueira</h1>`,
+    attachments: [
+        {
+            filename: arr,
+            path: path.join(__dirname, '/images'+ '/' + arr)
+        }
+      ]
+    }
+    
+    transporter.sendMail(email, (err, result) => {
+      if(err) return console.log(err)
+      console.log('Mensagem enviada!!!!')
+    })
     res.send("Single file upload success...")
 })
-
-const  transporter = nodemailer.createTransport({
-    service: SMTP_CONFIG.service,
-      auth: {
-        user: SMTP_CONFIG.user,
-        pass: SMTP_CONFIG.pass
-      }
-  });
-  
-const email = {
-  from: SMTP_CONFIG.user,
-  to: 'rdg6design@gmail.com',
-  subject: 'nodemailer + outlook',
-  text: 'email enviado com nodemailer.',
-  html: '<h1>You can send html formatted content using Nodemailer with attachments</h1>',
-  attachments: [
-      {
-          filename: arr.toString(),
-          path: path.join(__dirname, '/images'+ '/' + arr.toString())
-      }
-    ]
-  }
-  
-  transporter.sendMail(email, (err, result)=>{
-    if(err) return console.log(err)
-    console.log('Mensagem enviada!!!!')
-  })
-    
-
-// const directory = 'images';
-
-// fs.readdir(directory, (err, files) => {
-//   if (err) throw err;
-
-//   for (const file of files) {
-//     fs.unlink(path.join(directory, file), err => {
-//       if (err) throw err;
-//     });
-//   }
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
