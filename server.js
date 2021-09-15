0a55109ca112788ddf2531a4840138234deca815
Rodrigo Nogueira <rdg6design@gmail.com>
2021-09-15 15:22:56 -0300

    fix

--- server.js
+++ server.js
@@ -12,17 +12,16 @@ const port = 3000
 const dirPath = path.join(__dirname, '/images');
 
 const fileStorageengine = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './images')
-    },
+  destination: (req, file, cb) => {
+      cb(null, './images')
+  },
 
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "--" + file.originalname)
-    }
+  filename: (req, file, cb) => {
+      cb(null, Date.now() + "--" + file.originalname)
+  }
 })
   
 const upload = multer({ storage: fileStorageengine });
-console.log(upload, "upload")
 
 app.post('/single', upload.single('image'), (req, res)=> {
   const titulo = req.body.title
@@ -35,8 +34,8 @@ app.post('/single', upload.single('image'), (req, res)=> {
   const orderRecentFiles = (dirPath) => {
     return fs
       .readdirSync(dirPath)
-      .filter((file) => fs.lstatSync(path.join(dirPath, file)).isFile())
-      .map((file) => ({ file, mtime: fs.lstatSync(path.join(dirPath, file)).mtime }))
+      .filter(file => fs.lstatSync(path.join(dirPath, file)).isFile())
+      .map(file => ({ file, mtime: fs.lstatSync(path.join(dirPath, file)).mtime }))
       .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
   };
 
@@ -45,13 +44,13 @@ app.post('/single', upload.single('image'), (req, res)=> {
   console.log(getMostRecentFile(dirPath).file, 'file')
   console.log(getMostRecentFile(dirPath).mtime, 'mtime')
 
-    const  transporter = nodemailer.createTransport({
-      service: SMTP_CONFIG.service,
-        auth: {
-          user: SMTP_CONFIG.user,
-          pass: SMTP_CONFIG.pass
-        }
-    });
+  const  transporter = nodemailer.createTransport({
+    service: SMTP_CONFIG.service,
+      auth: {
+        user: SMTP_CONFIG.user,
+        pass: SMTP_CONFIG.pass
+      }
+  });
     
   const email = {
     from: SMTP_CONFIG.user,
@@ -60,17 +59,18 @@ app.post('/single', upload.single('image'), (req, res)=> {
     text: titulo,
     html: `<h1>Rodrigo Nogueira</h1>`,
     attachments: [
-        {
-            filename: arr,
-            path: path.join(__dirname, '/images'+ '/' + arr)
-        }
-      ]
-    }
+      {
+        filename: arr,
+        path: path.join(__dirname, `/images/${arr}`)
+      }
+    ]
+  }
     
-    transporter.sendMail(email, (err, result) => {
-      if(err) return console.log(err)
-      console.log('Mensagem enviada!!!!')
-    })
+  transporter.sendMail(email, (err, result) => {
+    if(err) return console.log(err)
+    console.log('E-mail enviado com sucesso!!!')
+  })
+  
     res.send("Single file upload success...")
 })
 
